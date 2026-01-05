c01808791ff64b8b4c799aae3504cf8c60d33efb
zakariadachi <dachiziko@gmail.com>
2026-01-05 10:16:00 +0100

    user repository

--- config/database.php
+++ config/database.php
@@ -9,3 +9,8 @@ return [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]
 ];
+
+define(DB_HOST, 'localhost');
+define(DB_NAME, 'photosphere');
+define(DB_USERNAME, 'root');
+define(DB_PASS, '');
